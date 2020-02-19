bbdfec619449fb0881ca2fd0a8808a2d95abc004
Guillaume Everarts de Velp <guillaume.everarts@student.uclouvain.be>
2020-02-19 18:28:48 +0100

    Adding warmup before measurements

--- benchmark.py
+++ benchmark.py
@@ -5,6 +5,12 @@ from podman import Podman
 from runc import RunC
 
 
+def warm_up(tool):
+    print('Warming up ' + str(tool.get_name()))
+    for i in range(1, 11):
+        tool.launch_many(i)
+
+
 def test_number(tool, rep=1, start=1, end=10, sync=True):
     for i in range(start, end + 1):
         total = 0
@@ -19,11 +25,8 @@ def test_number(tool, rep=1, start=1, end=10, sync=True):
 
 
 if __name__ == "__main__":
-    test_number(Docker(), rep=5, start=1, end=10, sync=True)
-    test_number(Docker(), rep=5, start=1, end=10, sync=False)
-    test_number(Inginious(), rep=5, start=1, end=10, sync=True)
-    test_number(Inginious(), rep=5, start=1, end=10, sync=False)
-    test_number(Podman(), rep=5, start=1, end=10, sync=True)
-    test_number(Podman(), rep=5, start=1, end=10, sync=False)
-    test_number(RunC(), rep=5, start=1, end=10, sync=True)
-    test_number(RunC(), rep=5, start=1, end=10, sync=False)
+    tools = [Docker(), Inginious(), Podman(), RunC()]
+    for tool in tools:
+        warm_up(tool)
+        test_number(tool, rep=5, start=1, end=10, sync=True)
+        test_number(tool, rep=5, start=1, end=10, sync=False)

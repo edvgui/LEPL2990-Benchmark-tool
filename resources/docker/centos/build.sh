288f07c199b4ec69ee433925ad0dd02fe89111b4
Guillaume Everarts de Velp <guillaume.everarts@student.uclouvain.be>
2020-03-16 19:32:35 +0100

    Scripting build of docker and podman containers

---
+++ resources/docker/centos/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
+LOG_FILE="${DIR}/build.log"
+
+build() {
+  local folder=$1
+
+  echo "Building centos-${folder}"
+  docker build -t "centos-${folder}" -f "${DIR}/${folder}/Dockerfile" "${DIR}/../../" &>> "${LOG_FILE}"
+}
+
+for arg in "$@"
+do
+  case ${arg} in
+    -a| --all)
+      for folder in $(cd ${DIR} && ls -d */ | cut -f1 -d'/')
+      do
+        build "${folder}"
+      done
+      ;;
+    *)
+      build "${arg}"
+      ;;
+  esac
+done

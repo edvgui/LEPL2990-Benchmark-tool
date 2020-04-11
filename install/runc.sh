5a41ac33dbccca1c725007db40f365f1146de152
Guillaume Everarts de Velp <guillaume.everarts@student.uclouvain.be>
2020-04-11 16:38:46 +0200

    Updating install scripts

--- install/runc.sh
+++ install/runc.sh
@@ -7,10 +7,18 @@ sudo -E add-apt-repository "deb [arch=${arch}] https://download.docker.com/linux
 sudo -E apt-get update
 sudo -E apt-get -y install docker-ce
 
-source /etc/os-release > /dev/null
-sudo sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
-sudo apt-get update -qq > /dev/null
-sudo apt-get -qq -y install podman > /dev/null
+sudo -E apt-get -y install \
+  uidmap \
+  golang-go
+
+cd /tmp || exit
+wget https://github.com/rootless-containers/rootlesskit/releases/download/v0.9.3/rootlesskit-x86_64.tar.gz
+tar -xzf rootlesskit-x86_64.tar.gz
+sudo mv rootlesskit /usr/bin/rootlesskit
+sudo mv rootlessctl /usr/bin/rootlessctl
+
+wget https://github.com/rootless-containers/slirp4netns/releases/download/v1.0.0/slirp4netns-x86_64
+sudo mv slirp4netns-x86_64 /usr/bin/slirp4netns
+sudo chmod +x /usr/bin/slirp4netns
 
 echo "net.ipv4.ping_group_range=0 2000000" | sudo tee /etc/sysctl.d/10-ipv4-ping.conf
